8ea67c20cf9209b7c83bf7da0a23417dc49a2ae7
DeveloperMan313 <053000voloshch@gmail.com>
2020-09-28 01:06:07 +0300

    Update sql_wrapper.py

--- server/src/sql_wrapper.py
+++ server/src/sql_wrapper.py
@@ -1,43 +1,132 @@
-import pymysql
+import sqlite3
 
 
 class SqlWrapper(object):
-    def __init__(self, host, user, password, bd_name, port):
-        self.conn = pymysql.Connect(host, user, password, bd_name, port)
+    def __init__(self, filename):
+        self.conn = sqlite3.connect(filename)
         self.curs = self.conn.cursor()
+        self.table = None
+        self.pk = None
 
     def __del__(self):
         self.conn.close()
 
-    def select(self, idx):
-        if not isinstance(idx, int) or idx < 1:
-            raise ValueError
-        self.curs.execute('SELECT report, score FROM reports WHERE id = ' + str(idx))
-        data = self.curs.fetchone()
-        return {'features': data[0], 'score': data[1]}
-
-    def length(self):
-        self.curs.execute('SELECT COUNT(id) FROM reports')
+    def __len__(self):
+        self.curs.execute(f'SELECT COUNT({self.pk}) FROM {self.table}')
         return self.curs.fetchone()[0]
 
-    def insert(self, row):
-        self.curs.execute(f'INSERT INTO reports VALUES (default, {row["features"]}, {row["score"]})')
+    def __getitem__(self, idx):
+        if self.__len__() == 0:
+            return None
+        if isinstance(idx, int):
+            if idx >= self.__len__() or idx < -self.__len__():
+                raise IndexError
+            if -self.__len__() <= idx < 0:
+                idx += self.__len__()
+            return self.curs.execute(f'SELECT * FROM {self.table} WHERE {self.pk} = {idx + 1}').fetchone()
+        elif isinstance(idx, slice):
+            start = idx.start
+            stop = idx.stop
+            if start is None:
+                start = 0
+            if stop is None:
+                stop = self.__len__()
+            if start >= self.__len__():
+                return []
+            if start < 0:
+                start += self.__len__()
+            while stop < 0:
+                stop += self.__len__()
+            if start >= stop:
+                return []
+            return self.curs.execute(
+                f'SELECT * FROM {self.table} WHERE {self.pk} BETWEEN {start + 1} AND {stop}').fetchall()[0:self.__len__():idx.step]
+        else:
+            raise TypeError
 
-    def delete(self, idx):
-        if not isinstance(idx, int) or idx < 1:
-            raise ValueError
-        self.curs.execute('DELETE FROM reports WHERE id = ' + str(idx))
+    def __setitem__(self, idx, row):
+        if self.__len__() == 0:
+            return
+        if isinstance(idx, int):
+            if isinstance(row, dict):
+                if idx >= self.__len__() or idx < -self.__len__():
+                    raise IndexError
+                if -self.__len__() <= idx < 0:
+                    idx += self.__len__()
+                for entry in zip(list(row), list(row.values())):
+                    if isinstance(entry[1], str):
+                        row[entry[0]] = '"' + entry[1] + '"'
+                    elif entry[1] is None:
+                        row[entry[0]] = 'NULL'
+                self.curs.execute(
+                    f'UPDATE {self.table} SET {", ".join([f"{list(row)[i]} = {list(row.values())[i]}" for i in range(len(row))])} WHERE {self.pk} = {idx + 1}')
+                self.conn.commit()
+            else:
+                raise TypeError
+        else:
+            raise TypeError
 
-    def update(self, row, idx):
-        if not isinstance(idx, int) or idx < 1:
-            raise ValueError
-        self.curs.execute(f'UPDATE reports SET report = "{row["features"]}", score = {row["score"]} WHERE id = {idx}')
+    def __delitem__(self, idx):
+        if self.__len__() == 0:
+            return
+        if isinstance(idx, int):
+            if idx >= self.__len__() or idx < -self.__len__():
+                raise IndexError
+            if -self.__len__() <= idx < 0:
+                idx += self.__len__()
+            self.curs.execute(f'DELETE FROM {self.table} WHERE {self.pk} = {idx + 1}')
+            self.conn.commit()
+        elif isinstance(idx, slice):
+            start = idx.start
+            stop = idx.stop
+            step = idx.step
+            if start is None:
+                start = 0
+            if stop is None:
+                stop = self.__len__()
+            if step is None:
+                step = 1
+            if start >= self.__len__():
+                return []
+            if start < 0:
+                start += self.__len__()
+            while stop < 0:
+                stop += self.__len__()
+            if start >= stop:
+                return []
+            self.curs.execute(f'DELETE FROM {self.table} WHERE {self.pk} BETWEEN {start + 1} AND {stop} AND {self.pk} % {step} = 1')
+            self.conn.commit()
+        else:
+            raise TypeError
+        ids = [list(row) for row in self.curs.execute(f'SELECT {self.pk} FROM {self.table}').fetchall()]
+        for i in enumerate(ids):
+            ids[i[0]].append(i[0] + 1)
+        ids = [[id2, id1] for id1, id2 in ids]
+        self.curs.executemany(f'UPDATE {self.table} SET {self.pk} = ? WHERE {self.pk} = ?', ids)
         self.conn.commit()
 
+    def __enter__(self):
+        return self.curs
 
-# db = SqlWrapper('host', 'username', 'password', 'db_name', 0)
-# print(db.select(1))
-# print(db.length())
-# db.insert({'features': 'да да да вы поняли', 'score': 0.5})
-# db.delete(1)
-# db.update({'features': 'че-то на языке рыбалки', 'score': 0.7}, 1)
+    def __exit__(self):
+        self.conn.close()
+
+    def set_table(self, table_name, pk):
+        self.table = table_name
+        self.pk = pk
+
+    def append(self, row):
+        if isinstance(row, dict):
+            if self.pk in list(row):
+                del row[self.pk]
+            row.update({self.pk: self.__len__() + 1})
+            for entry in zip(list(row), list(row.values())):
+                if isinstance(entry[1], str):
+                    row[entry[0]] = '"' + entry[1] + '"'
+                elif entry[1] is None:
+                    row[entry[0]] = 'NULL'
+            self.curs.execute(
+                f'INSERT INTO {self.table} ({", ".join([str(list(row)[i]) for i in range(len(row))])}) VALUES ({", ".join([str(list(row.values())[i]) for i in range(len(row))])})')
+            self.conn.commit()
+        else:
+            raise TypeError
